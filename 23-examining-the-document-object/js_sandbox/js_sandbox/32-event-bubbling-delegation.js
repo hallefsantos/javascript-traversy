111ba81ab63c5f70f6c572351013b846c19a2bba
Hallef Santos <hallefsantos@gmail.com>
2020-04-20 13:01:44 -0300

    32. Event Bubbling & Delegation

---
+++ 23-examining-the-document-object/js_sandbox/js_sandbox/32-event-bubbling-delegation.js
@@ -0,0 +1,43 @@
+// Event bubbling
+
+// document.querySelector('.card-title').addEventListener('click', 
+// function(){
+//     console.log('card title');
+// });
+
+// document.querySelector('.card-content').addEventListener('click', 
+// function(){
+//     console.log('card content');
+// });
+
+// document.querySelector('.card').addEventListener('click', 
+// function(){
+//     console.log('card');
+// });
+
+// document.querySelector('.col').addEventListener('click', 
+// function(){
+//     console.log('col');
+// });
+
+// Event delegation
+// const delItem = document.querySelector('.delete-item');
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e){
+    
+    // console.log(e.target);
+
+    // if(e.target.parentElement.className === 'delete-item secondary-content') {
+    //     console.log(e.target.parentElement);
+    //     console.log('delete item');
+    // }
+
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log(e.target.parentElement);
+        console.log('delete item');
+        e.target.parentElement.parentElement.remove();
+    }
+}
