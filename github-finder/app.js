9be21c9eb67117e20f4ad6423ac9e5178fb168f7
Hallef Santos <hallefsantos@gmail.com>
2020-05-06 23:23:08 -0300

    GitHub Finder

---
+++ github-finder/app.js
@@ -0,0 +1,35 @@
+// Init GitHub
+const github = new GitHub;
+// Init UI
+const ui = new UI;
+
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+// Search Input event listener
+searchUser.addEventListener('keyup', (e) => {
+   // get input text
+   const userText = e.target.value;
+
+   if(userText !== '') {
+      // console.log(usetText);
+      // Make http call
+      github.getUser(userText)
+         .then(data => {
+            if(data.profile.message === 'Not Found'){
+               //show alert
+               ui.showAlert('User not found', 'alert alert-danger');
+
+            }else {
+               // Show profile 
+               // console.log(data.profile);
+               ui.showProfile(data.profile);
+               ui.showRepos(data.repos);
+            }
+            
+         });
+   } else {
+      // Clear the profile
+      ui.clearProfile();
+   }
+});
