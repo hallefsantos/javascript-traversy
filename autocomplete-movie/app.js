0a7d70ecc000f2a2b0efd22157e0e09e2657d8a5
Hallef Santos <hallefsantos@gmail.com>
2020-05-22 23:21:43 -0300

    107. Add Total Calories

---
+++ autocomplete-movie/app.js
@@ -0,0 +1,118 @@
+const search = document.getElementById('search'),
+      matchList = document.getElementById('match-list'),
+      modal = document.getElementById('filme');
+
+// Event listener
+matchList.addEventListener('click', moreInfos);
+
+// Search movies.json and filter it
+const searchStates = async searchText => {
+   const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=ab872c8346e93a873401a241a30f8c44&query=${searchText}&include_adult=false`);
+
+   const movies = await res.json();
+
+   // Get Matches to current text input
+   let matches;
+
+   if (searchText.length >= 1) {
+      matches = movies.results.filter(movie => {
+         const regex = new RegExp(`^${searchText}`, 'gi');
+         return movie.title.match(regex);
+      });
+   } else {
+      matches = [];
+      matchList.innerHTML = '';
+   }
+   outputHtml(matches);
+};
+
+const outputHtml = matches => {
+   if (matches.length > 0) {
+      const poster = './movie.jpg';
+      const html = matches.map(match => `
+         <div class="card mb-3 px-md-2 px-0" id="${match.id}">
+            <div class="row no-gutters align-items-center">
+               <div class="col-lg-2 col-md-4 col-4">
+                  
+                  <img src="${(match.backdrop_path) ? "https://image.tmdb.org/t/p/w200/"+match.backdrop_path : poster}" class="card-img" alt="${match.title}">
+               </div>
+               <div class="col-lg-10 col-md-8 col-8">
+                  <div class="card-body">
+                  <h5 class="card-title">${match.title}</h5>
+                  <button type="button" class="btn btn-primary mt-2" data-toggle="modal" data-target="#filme">Details</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      `).join('');
+      matchList.innerHTML = html
+   }
+}
+
+function moreInfos(e){
+   const seeMoreBtn = e.target;
+
+   if(seeMoreBtn.classList.contains('btn')){
+      const card = seeMoreBtn.parentElement.parentElement.parentElement.parentElement;
+      const movieId = card.id;
+
+      searchMovie(movieId)
+         .then(data => fillModal(data));
+      
+   }
+}
+
+
+async function searchMovie(movieId) {
+   const res2 = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=ab872c8346e93a873401a241a30f8c44`);
+   const movie = await res2.json();
+   return (movie);
+}
+
+function fillModal(data){
+
+   let genres = '';
+   data.genres.forEach(genre => {
+      genres += `<span class="badge badge-pill badge-warning mx-1">${genre.name}</span>`;
+   });
+
+
+   let html = `
+
+      <div class="modal-content">
+         <div class="modal-header">
+         <h5 class="modal-title" id="filmeLabel">${data.original_title}</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+         </button>
+         </div>
+         <div class="modal-body">
+            <div class="row">
+               <div class="col-md-4">
+                  <img class="img-fluid mb-3 md-0" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="">
+               </div>
+               <div class="col-md-8">
+                  <p class="text-justify">${data.overview}</p>
+
+                  <div class="mb-3">${genres}</div>
+
+                  <ul class="list-group">
+                     <li class="list-group-item"><i class="fas fa-globe-europe"></i> Website: <a href="${data.homepage}" target="_blank">${data.homepage}</a></li>
+                     <li class="list-group-item"><i class="far fa-calendar-alt"></i> Release Data: ${data.release_date}</li>
+                     <li class="list-group-item"><i class="far fa-file-alt"></i> Tagline: ${data.tagline}</li>
+                     <li class="list-group-item"><i class="fas fa-poll"></i> Vote Average: ${data.vote_average}</li>
+                  </ul>
+                  
+               </>
+            </div>
+         </div>
+         <div class="modal-footer">
+         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+         </div>
+      </div>
+   `;
+
+   modal.querySelector('.modal-dialog').innerHTML = html;  
+}
+
+search.addEventListener('input', () => searchStates(search.value));
--- tracallory/app.js
+++ tracallory/app.js
@@ -1,51 +1,88 @@
 // Storage Controller
 
 // Item Controller
-const ItemCtrl = (function(){
-
+const ItemCtrl = (function () {
    // Item contructor
-   const Item = function(id, name, calories){
+   const Item = function (id, name, calories) {
       this.id = id;
       this.name = name;
       this.calories = calories;
-   }
+   };
 
    // Data Structure / State
    const data = {
       items: [
-         {id: 0, name: 'Steak Dinner', calories: 1200},
-         {id: 1, name: 'Cookie', calories: 200},
-         {id: 2, name: 'Eggs', calories: 600}
+         // {id: 0,name: "Steak Dinner",calories: 1200},
+         // {id: 1,name: "Cookie",calories: 200},
+         // {id: 2,name: "Eggs",calories: 600},
       ],
       currentItem: null,
-      totalCalories: 0
-   }
+      totalCalories: 0,
+   };
 
    // Public methods
    return {
-      getItems: function(){
+      getItems: function () {
          return data.items;
       },
-      logData: function(){
-         return data;
-      }
-   }
+      addItem: function (name, calories) {
+         let ID;
+         // Creat ID
+         if(data.items.length > 0){
+            ID = (data.items.length - 1) + 1;
+         }else {
+            ID = 0;
+         }
 
+         // Calories to Number
+         calories = parseInt(calories);
+         
+         // Create new Item
+         newItem = new Item(ID, name, calories);
+
+         // Add to items array
+         data.items.push(newItem);
+
+         return newItem;
+         
+         
+      },
+      getTotalCalories: function(){
+         let total = 0;
+
+         // loop through items and add cals
+         data.items.forEach(item => {
+            total += item.calories;
+         });
+         
+         //set total cal in data structure
+         data.totalCalories = total;
+
+         // return total
+         return data.totalCalories;
+      },
+      logData: function () {
+         return data;
+      },
+   };
 })();
 
 // UI Controller
-const UICtrl = (function(){
-
+const UICtrl = (function () {
    const UISelectors = {
-      itemList: '#item-list'
+      itemList: "#item-list",
+      addBtn: ".add-btn",
+      itemNameInput: '#item-name',
+      itemCaloriesInput: '#item-calories',
+      totalCalories: '.total-calories'
    };
-   
+
    // Public methods
    return {
-      populateItemsList: function(items){
-         let html = '';
+      populateItemsList: function (items) {
+         let html = "";
 
-         items.forEach(item => {
+         items.forEach((item) => {
             html += `
             <li class="collection-item" id="item-${item.id}">
                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
@@ -56,27 +93,108 @@ const UICtrl = (function(){
 
          // Insert list items
          document.querySelector(UISelectors.itemList).innerHTML = html;
-      }
-   }
-
+      },
+      getItemInput: function () {
+         return {
+            name: document.querySelector(UISelectors.itemNameInput).value,
+            calories: document.querySelector(UISelectors.itemCaloriesInput).value
+         }
+      },
+      addListItem: function(item){
+         // Show list
+         UICtrl.showList();
+         // Create li element
+         const li = document.createElement('li');
+         // add class
+         li.className = 'collection-item';
+         // Add ID
+         li.id = `item-${item.id}`;
+         // Add HTML
+         li.innerHTML = `
+            <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+         `;
+         // Insert Item
+         document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+      },
+      clearInput: function(){
+         document.querySelector(UISelectors.itemNameInput).value = '';
+         document.querySelector(UISelectors.itemCaloriesInput).value = '';
+      },
+      showList: function(){
+         // Show the list
+         document.querySelector(UISelectors.itemList).style.display = 'block';
+      },
+      hideList: function(){
+         document.querySelector(UISelectors.itemList).style.display = 'none';
+      },
+      showTotalCalories: function(total){
+         document.querySelector(UISelectors.totalCalories).textContent = total;
+      },
+      getSelectors: function () {
+         return UISelectors;
+      },
+   };
 })();
 
 // App Controller
-const App = (function(ItemCtrl, UICtrl){
+const App = (function (ItemCtrl, UICtrl) {
+   // Load event listeners
+   const loadEventListeners = function () {
+      // Get UI Selectors
+      const UISelectors = UICtrl.getSelectors();
+
+      // Add item event
+      document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+
+   };
+
+   // Add item submit
+   const itemAddSubmit = function (e) {
+      // Get form input from UI Controller
+      const input = UICtrl.getItemInput();
+
+      // Check for name and calorie input
+      if (input.name !== '' && input.calories !== '') {
+         // add item
+         const newItem = ItemCtrl.addItem(input.name, input.calories);
+         // Add to UI List
+         UICtrl.addListItem(newItem);
+
+         // Get total calories
+         const totalCalories = ItemCtrl.getTotalCalories();
+
+         // add totalCalories to UI
+         UICtrl.showTotalCalories(totalCalories);
+         
+         // Clear fiels
+         UICtrl.clearInput();
+      }
+
+      e.preventDefault();
+   }
 
    // Public methods
    return {
-      init: function(){
+      init: function () {
          // console.log('Initializing App...');
 
          // Fetch items from data structure
          const items = ItemCtrl.getItems();
 
-         // Populate list with Items
-         UICtrl.populateItemsList(items);
-      }
-   }
+         // Check if any items
+         if(items.length === 0){
+            UICtrl.hideList();
+         } else {
+            // Populate list with Items
+            UICtrl.populateItemsList(items);
+         } 
 
+         // Load all event listeners
+         loadEventListeners();
+      },
+   };
 })(ItemCtrl, UICtrl);
 
 App.init();
